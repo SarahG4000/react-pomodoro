860cec77f64e72b62cf2fe70eeb401d43038eef9
SarahG4000 <sarah.guillaume4000@gmail.com>
2020-05-20 16:30:16 +0200

    debut du code pour preparer un timer

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 /* creation d'un pomodoro
- * /src/applicationCache.js - container for app
+ * /src/app.js - container for app
  * code by sarahG4000 for becode formation
  * creation for montagne
  */
-console.warn("hello pomodoro");
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Pomodoro from "./components/pomodoro";
+
+ReactDOM.render(<Pomodoro />, document.querySelector("#app"));
---
+++ src/components/pomodoro.js
@@ -0,0 +1,14 @@
+/* creation d'un pomodoro
+ * /src/pomodoro.js - container for app
+ * code by sarahG4000 for becode formation
+ * creation for montagne
+ */
+import React from "react";
+
+const Pomodoro = () => (
+    <div>
+        <p>{"hello pomodoro"}</p>
+    </div>
+);
+
+export default Pomodoro;
