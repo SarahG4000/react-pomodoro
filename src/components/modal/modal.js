eefa42599255dab87a487667a95bd74dad01c501
SarahG4000 <sarah.guillaume4000@gmail.com>
2020-05-24 22:48:25 +0200

    mise en place du modal

---
+++ src/components/modal/modal.js
@@ -0,0 +1,75 @@
+/* creation d'un pomodoro
+ * /src/components/modal/modal.js - modal fot timer
+ * code by sarahG4000 for becode formation
+ * creation for montagne
+ * merci leny pour cette bonne pratique,
+ * ça me permet de mieux comprendre ce que je fais et de decouper monn code pour m'y retrouvée
+ */
+import React from "react";
+import PropTypes from "prop-types";
+import {createPortal} from "react-dom";
+
+import useTimer from "../../seed/hooks/use-timer";
+
+import Button from "../tools/button";
+import Display from "../display/display";
+
+import {NBSP, SESSION_TIMER_PAUSE} from "../../seed/constants";
+
+const containerStyles = {
+    position: "fixed",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    display: "flex",
+    zIndex: 1000,
+    justifyContent: "center",
+    alignItems: "center",
+    background: "rgba(0,0,0,0.5)",
+};
+
+const Modal = ({show = false, onClose, onRestart}) => {
+    if (!show) {
+        return null;
+    }
+
+    const [{running, seconds}, {setRunning}] = useTimer(
+        SESSION_TIMER_PAUSE,
+        true,
+        onRestart,
+    );
+
+    const stopThen = next => () => {
+        setRunning(false);
+        next();
+    };
+
+    return createPortal(
+        <div style={containerStyles}>
+            <div className={"box"}>
+                <h4> {"It's over!"} </h4>
+                <p> {"Please, take a short break."} </p>
+                <p>
+                    {
+                        "When the pause timer will be over, a new session will start."
+                    }
+                </p>
+                <Display seconds={seconds} running={running} />
+                <div className={"is-flex"}>
+                    <Button label={"Stop"} onClick={stopThen(onClose)} /> {NBSP}
+                    <Button label={"Restart"} onClick={stopThen(onRestart)} />
+                </div>
+            </div>
+        </div>,
+        document.querySelector("#modals"),
+    );
+};
+
+Modal.propTypes = {
+    show: PropTypes.bool,
+    onClose: PropTypes.func.isRequired,
+    onRestart: PropTypes.func.isRequired,
+};
+
+export default Modal;
--- src/components/pomodoro.js
+++ src/components/pomodoro.js
@@ -5,18 +5,21 @@
  * merci leny pour cette bonne pratique,
  * ça me permet de mieux comprendre ce que je fais et de decouper monn code pour m'y retrouvée
  */
-import React, {useCallback} from "react";
+import React, {useState, useCallback} from "react";
 import {SESSION_TIMER} from "../seed/constants";
 
 import Display from "./display/display";
 import Tools from "./tools/tools";
 import useTimer from "../seed/hooks/use-timer";
+import Modal from "./modal/modal";
 
 const Pomodoro = () => {
-    const [{seconds, running}, {setSeconds, setRunning}] = useTimer(
-        SESSION_TIMER,
-        false,
-    );
+    const [showModal, setShowModal] = useState(false);
+    const [
+        {seconds, running},
+        {setSeconds, setRunning},
+    ] = useTimer(SESSION_TIMER, false, () => setShowModal(true));
+
     //ou setSeconds(Math.max(seconds-60,0));
     const handleMinus = useCallback(
         () => setSeconds(seconds < 60 ? 0 : seconds - 60),
@@ -33,6 +36,16 @@ const Pomodoro = () => {
     const handleReset = useCallback(() => setSeconds(SESSION_TIMER), [
         setSeconds,
     ]);
+    const handleModalClose = useCallback(() => {
+        setShowModal(false);
+        handleReset();
+    }, [setShowModal, handleReset]);
+
+    const handleRestart = useCallback(() => {
+        handleModalClose();
+        handleStartBreak();
+    }, [handleModalClose, handleStartBreak]);
+
     return (
         <div className={["columns", "is-mobile", "is-centered"].join(" ")}>
             <div className={["column", "is-half"].join(" ")}>
@@ -45,8 +58,12 @@ const Pomodoro = () => {
                     onReset={handleReset}
                 />
             </div>
+            <Modal
+                show={showModal}
+                onClose={handleModalClose}
+                onRestart={handleRestart}
+            />
         </div>
     );
 };
-
 export default Pomodoro;
